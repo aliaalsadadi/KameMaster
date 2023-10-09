f39fa48fb96504c22e1ee4b7ee3be9aa7326bc23
aliaalsadadi <aliaalsadadi@outlook.com>
2023-10-09 21:56:53 +0300

    Initial commit

---
+++ src/HelloCV.java
@@ -0,0 +1,7 @@
+public class HelloCV {
+    public static void main(String[] args){
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
+    }
+}
